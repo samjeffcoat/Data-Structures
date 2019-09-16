a2e6fdff30927809f00f182fdb0898f4150ed5fe
Sam <Jeffcoat>
2019-09-16 15:36:01 -0500

    Adds en que method

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,18 +1,22 @@
+from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+
 
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # Why is our DLL a good choice to store our elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
 
-  def enqueue(self, value):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, value):
+       """ Adds an item into our que
+       """
+       self.size +=1
+       self.storage.add_to_tail(value)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        pass
+
+    def len(self):
+        pass
