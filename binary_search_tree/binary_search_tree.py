78bd0f53f9eb3f7dd743d819f99c624a7d5c74dc
Sam <Jeffcoat>
2019-09-19 14:44:59 -0500

    MVP complete for yesterday with refactor and callback

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,8 +1,8 @@
-
+import sys
+sys.path.append('../queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
-import sys
-sys.path.append('./queue_and_stack')
+
 
 # Questions:
 # Only ints?
@@ -15,43 +15,58 @@ sys.path.append('./queue_and_stack')
 
 
 class BinarySearchTree:
-  def __init__(self, value):  # We're just using value, so key is value
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):  # We're just using value, so key is value
+        self.value = value
+        self.left = None
+        self.right = None
 
-  # * `insert` adds the input value to the binary search tree, adhering to the
-  # rules of the ordering of elements in a binary search tree.
-  # Need to traverse to find spot to insert
-  def insert(self, value):
-    new_tree = BinarySearchTree(value)
-    if value < self.value:
-      if self.left == None:
-        self.left = new_tree
-      else:
-        self.left.insert(value)
-    else:
-        if self.right == None:
-          self.right == new_tree
-
+    # * `insert` adds the input value to the binary search tree, adhering to the
+    # rules of the ordering of elements in a binary search tree.
+    # Need to traverse to find spot to insert
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
         else:
-            self.right.insert(value)
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
+    # * `contains` searches the binary search tree for the input value,
+    # returning a boolean indicating whether the value exists in the tree or not.
+    # Start from root and traverse the tree
+    # We can stop at the first instance of a value
+    # We know it's not found if we get to a node that doesn't have children
 
-  # * `contains` searches the binary search tree for the input value,
-  # returning a boolean indicating whether the value exists in the tree or not.
-  # Start from root and traverse the tree
-  # We can stop at the first instance of a value
-  # We know it's not found if we get to a node that doesn't have children
-  def contains(self, target):
-      if target == value
+    def contains(self, target):
+        if target == self.value:
+            return True
+        else:
+            if target < self.value:
+                if self.left == None:
+                    return False
+                return self.left.contains(target)
+            else:
+                if self.right == None:
+                    return False
+                return self.right.contains(target)
 
-  # * `get_max` returns the maximum value in the binary search tree.
-  def get_max(self):
-    pass
+    # * `get_max` returns the maximum value in the binary search tree.
+    def get_max(self):
+        if self.right == None:
+            return self.value
+        else:
+            return self.right.get_max()
 
-  # * `for_each` performs a traversal of _every_ node in the tree, executing
-  # the passed-in callback function on each tree node value. There is a myriad of ways to
-  # perform tree traversal; in this case any of them should work.
-  def for_each(self, cb):
-    pass
+    # * `for_each` performs a traversal of _every_ node in the tree, executing
+    # the passed-in callback function on each tree node value. There is a myriad of ways to
+    # perform tree traversal; in this case any of them should work.
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
